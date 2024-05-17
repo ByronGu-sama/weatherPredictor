848499a06443d9f75fa68dfd7ac5e3cbf1ccb842
hanserr <2277165192@qq.com>
2024-05-17 16:04:10 +0800

    add two files

---
+++ src/components/controller/controller.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import {ref, watch} from "vue";
+import commonUtils from "../../utils/commonUtils.ts";
+import {alert} from "/src/utils/testDataList.ts"
+
+let notification = ref(false);
+
+const pushAlert = () => {
+  let temp = []
+  for(let i of alert.warning){
+    temp.push({
+      title:i.title,
+      content:i.text,
+      type:'warning',
+    })
+  }
+  commonUtils.pushNotification(temp,8000)
+}
+
+watch(notification,(n) => {
+  if(n){
+    pushAlert()
+  }
+})
+
+</script>
+
+<template>
+  <div class="controller-body">
+    天气预警测试&emsp;<el-switch size="small" v-model="notification" />
+  </div>
+</template>
+
+<style scoped>
+.controller-body{
+  width: 125px;
+  height: 24px;
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  border-radius: 10px;
+  border: 1px solid rgba(227, 227, 227, 0.39);
+  font-size: 12px;
+}
+</style>
---
+++ src/utils/testDataList.ts
@@ -0,0 +1,645 @@
+export const typhoon =  {
+    "code": "200",
+    "updateTime": "2021-04-21T02:34+00:00",
+    "fxLink": "https://www.qweather.com",
+    "isActive": "0",
+    "track": [
+        {
+            "time": "2020-08-08T08:00+08:00",
+            "lat": "17",
+            "lon": "126.7",
+            "type": "TD",
+            "pressure": "1000",
+            "windSpeed": "15",
+            "moveSpeed": "25",
+            "moveDir": "北",
+            "move360": ""
+        },
+        {
+            "time": "2020-08-08T11:00+08:00",
+            "lat": "17.2",
+            "lon": "126.8",
+            "type": "TD",
+            "pressure": "1000",
+            "windSpeed": "15",
+            "moveSpeed": "30",
+            "moveDir": "北",
+            "move360": ""
+        },
+        {
+            "time": "2020-08-08T14:00+08:00",
+            "lat": "17.7",
+            "lon": "126.7",
+            "type": "TD",
+            "pressure": "1000",
+            "windSpeed": "15",
+            "moveSpeed": "40",
+            "moveDir": "北",
+            "move360": ""
+        },
+        {
+            "time": "2020-08-08T17:00+08:00",
+            "lat": "18.1",
+            "lon": "126.6",
+            "type": "TD",
+            "pressure": "1000",
+            "windSpeed": "15",
+            "moveSpeed": "40",
+            "moveDir": "北",
+            "move360": ""
+        },
+        {
+            "time": "2020-08-08T20:00+08:00",
+            "lat": "18.6",
+            "lon": "126.5",
+            "type": "TD",
+            "pressure": "1000",
+            "windSpeed": "15",
+            "moveSpeed": "45",
+            "moveDir": "北",
+            "move360": ""
+        },
+        {
+            "time": "2020-08-08T23:00+08:00",
+            "lat": "19.4",
+            "lon": "126.4",
+            "type": "TD",
+            "pressure": "1000",
+            "windSpeed": "15",
+            "moveSpeed": "50",
+            "moveDir": "北",
+            "move360": ""
+        },
+        {
+            "time": "2020-08-09T02:00+08:00",
+            "lat": "20.5",
+            "lon": "126.3",
+            "type": "TS",
+            "pressure": "998",
+            "windSpeed": "18",
+            "moveSpeed": "45",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "180",
+                "seRadius": "350",
+                "swRadius": "250",
+                "nwRadius": "150"
+            }
+        },
+        {
+            "time": "2020-08-09T05:00+08:00",
+            "lat": "21.5",
+            "lon": "126.3",
+            "type": "TS",
+            "pressure": "998",
+            "windSpeed": "18",
+            "moveSpeed": "45",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "180",
+                "seRadius": "350",
+                "swRadius": "250",
+                "nwRadius": "150"
+            }
+        },
+        {
+            "time": "2020-08-09T08:00+08:00",
+            "lat": "22.9",
+            "lon": "126.3",
+            "type": "TS",
+            "pressure": "998",
+            "windSpeed": "18",
+            "moveSpeed": "49",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "180",
+                "seRadius": "350",
+                "swRadius": "250",
+                "nwRadius": "150"
+            }
+        },
+        {
+            "time": "2020-08-09T09:00+08:00",
+            "lat": "23.3",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "995",
+            "windSpeed": "20",
+            "moveSpeed": "49",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "180",
+                "seRadius": "350",
+                "swRadius": "150",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-09T10:00+08:00",
+            "lat": "23.7",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "995",
+            "windSpeed": "20",
+            "moveSpeed": "49",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "180",
+                "seRadius": "350",
+                "swRadius": "150",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-09T11:00+08:00",
+            "lat": "24.1",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "995",
+            "windSpeed": "20",
+            "moveSpeed": "46",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "150",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-09T12:00+08:00",
+            "lat": "24.5",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "995",
+            "windSpeed": "20",
+            "moveSpeed": "46",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "150",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-09T13:00+08:00",
+            "lat": "24.9",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "995",
+            "windSpeed": "20",
+            "moveSpeed": "46",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "150",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-09T14:00+08:00",
+            "lat": "25.4",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "995",
+            "windSpeed": "20",
+            "moveSpeed": "46",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-09T15:00+08:00",
+            "lat": "25.8",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "995",
+            "windSpeed": "20",
+            "moveSpeed": "46",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-09T16:00+08:00",
+            "lat": "26.2",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "995",
+            "windSpeed": "20",
+            "moveSpeed": "46",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-09T17:00+08:00",
+            "lat": "26.4",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "44",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-09T18:00+08:00",
+            "lat": "26.6",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "44",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-09T19:00+08:00",
+            "lat": "26.8",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "45",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-09T20:00+08:00",
+            "lat": "27.2",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "45",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-09T21:00+08:00",
+            "lat": "27.6",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "45",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-09T22:00+08:00",
+            "lat": "28",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "45",
+            "moveDir": "北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-09T23:00+08:00",
+            "lat": "28.5",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "46",
+            "moveDir": "北东北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-10T00:00+08:00",
+            "lat": "29",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "46",
+            "moveDir": "北东北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-10T01:00+08:00",
+            "lat": "29.4",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "45",
+            "moveDir": "北东北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-10T02:00+08:00",
+            "lat": "29.7",
+            "lon": "126.2",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "47",
+            "moveDir": "北东北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-10T03:00+08:00",
+            "lat": "30",
+            "lon": "126.3",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "47",
+            "moveDir": "北东北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-10T04:00+08:00",
+            "lat": "30.4",
+            "lon": "126.4",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "47",
+            "moveDir": "北东北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-10T05:00+08:00",
+            "lat": "30.7",
+            "lon": "126.5",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "52",
+            "moveDir": "北东北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-10T06:00+08:00",
+            "lat": "31.3",
+            "lon": "126.7",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "52",
+            "moveDir": "北东北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-10T08:00+08:00",
+            "lat": "32",
+            "lon": "127.6",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "53",
+            "moveDir": "北东北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "350",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-10T11:00+08:00",
+            "lat": "33.2",
+            "lon": "128",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "52",
+            "moveDir": "北东北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "300",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-10T14:00+08:00",
+            "lat": "34.6",
+            "lon": "128.6",
+            "type": "TS",
+            "pressure": "992",
+            "windSpeed": "23",
+            "moveSpeed": "58",
+            "moveDir": "北东北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "300",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        },
+        {
+            "time": "2020-08-10T17:00+08:00",
+            "lat": "35.9",
+            "lon": "129.3",
+            "type": "TS",
+            "pressure": "998",
+            "windSpeed": "20",
+            "moveSpeed": "59",
+            "moveDir": "北东北",
+            "move360": "",
+            "windRadius30": {
+                "neRadius": "220",
+                "seRadius": "300",
+                "swRadius": "100",
+                "nwRadius": "100"
+            }
+        }
+    ],
+    "refer": {
+        "sources": [
+            "QWeather"
+        ],
+        "license": [
+            "QWeather Developers License"
+        ]
+    }
+}
+
+export const alert = {
+    "code": "200",
+    "updateTime": "2024-05-14T20:34+08:00",
+    "fxLink": "https://www.qweather.com/en/severe-weather/shuicheng-101260801.html",
+    "warning": [
+        {
+            "id": "10126080120240514180500525980785",
+            "sender": "南通市住房和城乡建设局",
+            "pubTime": "2024-05-14T18:05+08:00",
+            "title": "南通市住房和城乡建设局发布内涝黄色预警[III级/较重]",
+            "startTime": "2024-05-14T17:57+08:00",
+            "endTime": "2024-05-15T17:57+08:00",
+            "status": "active",
+            "level": "黄色",
+            "severity": "Moderate",
+            "severityColor": "Yellow",
+            "type": "1202",
+            "typeName": "内涝",
+            "urgency": "",
+            "certainty": "",
+            "text": "南通市住房城乡建设局、应急局、气象局2024年5月14日18时05分联合发布城镇内涝气象风险黄色预警信号：预计今天傍晚到夜间，我区雨量普遍中到大雨，中南部部分乡镇有大雨到暴雨，上述地区累积降雨量普遍有40～60毫米，过程中最大小时雨强可达30～50毫米，将导致上述城区和乡镇低洼地段出现积水，对交通影响较大，区住建局、应急局、气象局联合提醒，请注意加强防范。",
+            "related": ""
+        },
+        {
+            "id": "10126080120240514173413671961046",
+            "sender": "南通市气象台",
+            "pubTime": "2024-05-14T17:34+08:00",
+            "title": "南通市气象台发布暴雨黄色预警[Ⅲ/较重]",
+            "startTime": "2024-05-14T17:34+08:00",
+            "endTime": "2024-05-15T17:34+08:00",
+            "status": "active",
+            "level": "黄色",
+            "severity": "Moderate",
+            "severityColor": "Yellow",
+            "type": "1003",
+            "typeName": "暴雨",
+            "urgency": "",
+            "certainty": "",
+            "text": "南通市气象台2024年5月14日17时34分发布暴雨黄色预警信号：目前我区营盘乡降雨量已达42.6毫米且降雨仍将持续，预计未来6小时，我区中南部乡镇将出现暴雨天气，请注意防范强降水引发的城乡积涝、山洪、小流域洪水、山体滑坡等灾害。",
+            "related": ""
+        },
+        {
+            "id": "10126080120240514143523098089052",
+            "sender": "南通市气象台",
+            "pubTime": "2024-05-14T14:35+08:00",
+            "title": "南通市气象台发布雷电黄色预警[Ⅲ/较重]",
+            "startTime": "2024-05-14T14:35+08:00",
+            "endTime": "2024-05-15T14:35+08:00",
+            "status": "active",
+            "level": "黄色",
+            "severity": "Moderate",
+            "severityColor": "Yellow",
+            "type": "1014",
+            "typeName": "雷电",
+            "urgency": "",
+            "certainty": "",
+            "text": "南通市气象台2024年5月14日14时35分发布雷电黄色预警信号：预计未来6小时，我区坪寨、玉舍、海坪、都格等中西部乡镇（街道）将受雷电天气影响，局地伴有大风、小冰雹、短时强降水等强对流天气，请注意防范。",
+            "related": ""
+        }
+    ],
+    "refer": {
+        "sources": [
+            "国家预警信息发布中心 (12379)"
+        ],
+        "license": [
+            "QWeather Developers License"
+        ]
+    }
+}
